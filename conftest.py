dc36b26cc0da9b56fe26d0c5e647b5573a1489fc
Tom Ostin <tm.proquestions@gmail.com>
2025-05-29 14:45:57 +0300

    Fix: добавлены триггеры on: для запуска GitHub Actions

--- conftest.py
+++ conftest.py
@@ -6,21 +6,30 @@ from datetime import datetime
 import os
 
 def pytest_addoption(parser):
-    # Если пользователь передал --headed, то отключаем headless
+    """
+    Добавляет опцию командной строки '--headed' для запуска браузера с GUI.
+    По умолчанию тесты запускаются в headless-режиме.
+    """
     parser.addoption("--headed", action="store_true", help="Открыть браузер с GUI (отключить headless)")
 
 @pytest.fixture
 def driver(request):
+    """
+    Фикстура инициализирует WebDriver для каждого теста.
+    Запускается в headless-режиме, если не указан флаг --headed.
+    В случае падения теста делает скриншот и сохраняет его в папку /screenshots.
+    """
     headed = request.config.getoption("--headed")
 
-    chrome_driver_path = r"E:\Program Files\chromedriver-win64\chromedriver.exe"
-    service = Service(executable_path=chrome_driver_path)
+    # ChromeDriver будет найден, если добавлен в системный PATH
+    service = Service()
 
     options = Options()
-
     if not headed:
         options.add_argument("--headless=new")
         options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--log-level=3")
@@ -28,6 +37,7 @@ def driver(request):
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
 
+    # Если тест упал — сохраняем скриншот
     if request.node.rep_call.failed:
         time_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         filename = f"screenshots/FAIL_{request.node.name}_{time_str}.png"
@@ -37,9 +47,12 @@ def driver(request):
 
     driver.quit()
 
-# Хук для фиксации результатов и передачи в фикстуру
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
+    """
+    Хук pytest: сохраняет объект результата выполнения теста,
+    чтобы можно было узнать статус изнутри фикстур (например, для скриншота).
+    """
     outcome = yield
     rep = outcome.get_result()
     setattr(item, f"rep_{rep.when}", rep)
